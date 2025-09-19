33a493e69ad63e01d7e29f0894eeac0c21a0edcd
Alexandre <alexandre@macbookair.home>
2025-09-19 10:58:54 +0200

    Fix CORS configuration - explicitly allow tenderpulse.eu and www.tenderpulse.eu origins

--- app.py
+++ app.py
@@ -48,9 +48,14 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://tenderpulse.eu",
+        "https://www.tenderpulse.eu",
+        "http://localhost:3000",
+        "http://localhost:3001"
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
